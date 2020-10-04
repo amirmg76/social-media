b6068aef6d2d58e1efe723e18a32194d4a39eabb
amir ghazanfari <amir_ghazanfari@outlook.com>
2020-10-04 13:39:33 +0330

    solve the problem with static and template

--- social_media/social_media/settings.py
+++ social_media/social_media/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'social_media.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,10 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'meida')
-
-TEMPLATE_URL = '/template/'
-TEMPLATE_ROOT = os.path.join(BASE_DIR, 'template')
